2ba76549d0eda623b6beffbe403a498951bd61db
Omniya <Omniya@DESKTOP-7E9LGFU>
2025-03-25 19:23:20 +0200

    Add project files.

---
+++ Company.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Company
+    {
+        private List<Department> Departments;
+        private List<Employee> Employees;
+
+        public Company()
+        {
+            Departments = new List<Department>();
+            Employees = new List<Employee>();
+        }
+
+        public void AddDepartment(Department dept)
+        {
+            Departments.Add(dept);
+            Console.WriteLine($" Department {dept.Name} has been added to the company.");
+        }
+
+        public void AddEmployee(Employee emp, string deptName)
+        {
+            var dept = Departments.Find(d => d.Name == deptName);
+            if (dept != null)
+            {
+                dept.AddEmployee(emp);
+                Employees.Add(emp);
+            }
+            else
+            {
+                Console.WriteLine(" Department not found.");
+            }
+        }
+        public void GenerateReport()
+        {
+            Console.WriteLine("\n Employee Report:");
+            foreach (var dept in Departments)
+            {
+                Console.WriteLine($"\n {dept.Name}");
+                dept.ListEmployees();
+
+            }
+        }
+
+
+
+    }
+}
